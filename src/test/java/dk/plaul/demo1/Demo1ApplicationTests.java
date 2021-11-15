decd07d3decbc1d3b0f31191ec4820ff0dd630fa
plaul <lmor@kea.dk>
2021-11-15 10:03:06 +0100

    dummy test

--- src/test/java/dk/plaul/demo1/Demo1ApplicationTests.java
+++ src/test/java/dk/plaul/demo1/Demo1ApplicationTests.java
@@ -1,13 +1,16 @@
 package dk.plaul.demo1;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Demo1ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+    assertEquals(5, 5);
+  }
 
 }
